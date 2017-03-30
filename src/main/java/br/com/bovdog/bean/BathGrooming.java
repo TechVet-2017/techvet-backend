aa68b112e332456b848c14398f40fb26cfcfa666
Varley <varleysilva@me.com>
2017-03-29 22:06:30 -0300

    Maintain Bath and Grooming Backend
    Creating gets and sets
    
    Related to issue: C2UC4_maintain_bathGrooming_backend
    
    Signed-off-by: varleysilva  <varleysilva@me.com>
    Singned-off-by: gustavo2795 <gustavo.unb@gmail.com>

--- src/main/java/br/com/bovdog/bean/BathGrooming.java
+++ src/main/java/br/com/bovdog/bean/BathGrooming.java
@@ -1,6 +1,21 @@
 package br.com.bovdog.bean;
 
 public class BathGrooming {
-	private int id;
-	private String service;
+	private int idBathGrooming;
+	private String serviceBathGrooming;
+	
+	public int getIdBathGrooming() {
+		return idBathGrooming;
+	}
+	public void setIdBathGrooming(int idBathGrooming){
+		this.idBathGrooming = idBathGrooming;
+	}
+	
+	public String getServiceBathGrooming() {
+		return serviceBathGrooming;
+	}
+	public void setServiceBathGrooming(String serviceBathGrooming){
+		this.serviceBathGrooming = serviceBathGrooming;
+	}
+	
 }
