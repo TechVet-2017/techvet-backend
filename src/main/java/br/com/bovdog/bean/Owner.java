0e5e88d5a4210d742f4fded82729dea195e83504
João Paulo Ferreira <iamferreirajp@gmail.com>
2017-03-29 22:21:02 -0300

    Implement owner class Create the owner bean Related to #6 Signed-off-by: João Paulo Ferreira <iamferreirajp@gmail.com> Signed-off-by: Adaílson Santos <adailson2@gmail.com>

---
+++ src/main/java/br/com/bovdog/bean/Owner.java
@@ -0,0 +1,44 @@
+package br.com.bovdog.bean;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Owner {
+
+	private Long cpf;
+	private String ownerName;
+	private String ownerLastName;
+	private Long phoneNumber;
+	private String address;
+	
+	public Long getCpf() {
+		return cpf;
+	}
+	public void setCpf(Long cpf) {
+		this.cpf = cpf;
+	}
+	public String getOwnerName() {
+		return ownerName;
+	}
+	public void setOwnerName(String ownerName) {
+		this.ownerName = ownerName;
+	}
+	public String getOwnerLastName() {
+		return ownerLastName;
+	}
+	public void setOwnerLastName(String ownerLastName) {
+		this.ownerLastName = ownerLastName;
+	}
+	public Long getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(Long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
