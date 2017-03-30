edd624b8f9577221c159626e26d94da575f5b9e8
Flavio Costa <skingk.f@gmail.com>
2017-03-29 21:08:10 -0300

    Create Patient model java bean
    Related to issue #4
    
    Signed-off-by: Flavio Costa <skingk.f@gmail.com>
    Signed-off-by: Leonardo Sagmeister <leomeister22@gmail.com>

---
+++ src/main/java/br/com/bovdog/bean/Patient.java
@@ -0,0 +1,66 @@
+package br.com.bovdog.bean;
+
+import java.util.Date;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Patient {
+	private String name;
+	private String species;
+	private String race;
+	private char size;
+	private char gender;
+	private Date birthDate;
+	private String furCharacteristics;
+	private float weight;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSpecies() {
+		return species;
+	}
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+	public String getRace() {
+		return race;
+	}
+	public void setRace(String race) {
+		this.race = race;
+	}
+	public char getSize() {
+		return size;
+	}
+	public void setSize(char size) {
+		this.size = size;
+	}
+	public char getGender() {
+		return gender;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	public String getFurCharacteristics() {
+		return furCharacteristics;
+	}
+	public void setFurCharacteristics(String furCharacteristics) {
+		this.furCharacteristics = furCharacteristics;
+	}
+	public float getWeight() {
+		return weight;
+	}
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+	
+}
