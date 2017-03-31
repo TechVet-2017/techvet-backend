4d5428fae44bfe668acf9924aacc8693fa5cb5f9
João Paulo Ferreira <iamferreirajp@gmail.com>
2017-03-31 18:39:47 -0300

    Add ownerId attribute to Owner Change the migration file to match the ownerId Related to issue #6 Signed-off-by: João Paulo Ferreira <iamferreirajp@gmail.com> Signed-off-by: Adailson Santos <adailson2@gmail.com>

--- src/main/java/br/com/bovdog/bean/Owner.java
+++ src/main/java/br/com/bovdog/bean/Owner.java
@@ -5,12 +5,19 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class Owner {
 
-	private Long cpf;
-	private String ownerName;
+	private int ownerId;
+	private Long cpf; //Individual Registry in Brazil
+	private String ownerName; 
 	private String ownerLastName;
 	private Long phoneNumber;
 	private String address;
 	
+	public int getOwnerId() {
+		return ownerId;
+	}
+	public void setOwnerId(int ownerId) {
+		this.ownerId = ownerId;
+	}
 	public Long getCpf() {
 		return cpf;
 	}
