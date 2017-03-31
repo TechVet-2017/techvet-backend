c86cd35bcf46514de9c19d315905cc8d84895e1d
João Paulo Ferreira <iamferreirajp@gmail.com>
2017-03-31 00:48:58 -0300

    Add owner seed and fix owner migrate Fix getAllOwners method in ownerDAO Related to #6 Signed-off-by: João Paulo Ferreira <iamferreirajp@gmail.com> Signed-off-by: Adailson Santos <adailson2@gmail.com>

--- src/main/resources/db/migration/V1.1.1__Create_owner_table.sql
+++ src/main/resources/db/migration/V1.1.1__Create_owner_table.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS `owner`;
 CREATE TABLE `owner`(
-	`cpf` int(10) NOT NULL,
+	`cpf` bigint(11) NOT NULL,
 	`ownerName` varchar(50) NOT NULL,
 	`ownerLastName` varchar(50) NOT NULL,
-	`phoneNumber` int(20) NOT NULL,
+	`phoneNumber` bigint(20) NOT NULL,
  	`address` varchar(200) NOT NULL,
  	PRIMARY KEY(`cpf`)
 );
---
+++ src/main/resources/db/migration/V1.1.2__Seed_owner_table.sql
@@ -0,0 +1,4 @@
+INSERT INTO `owner` (cpf, ownerName, ownerLastName, phoneNumber, address ) VALUES (11111111111,"João Paulo", "Ferreira", 62982182377, "Rua das Maravilhas Número 10 Lote");
+INSERT INTO `owner` (cpf, ownerName, ownerLastName, phoneNumber, address ) VALUES (22222222222,"Adalto", "Santos", 61999999999, "Rua das Acácias Número 23 Lote 02");
+INSERT INTO `owner` (cpf, ownerName, ownerLastName, phoneNumber, address ) VALUES (33333333333,"Nicácio", "Estagiário", 61111111111, "CPD da FGA Sala 10 das 10h as 17h");
+INSERT INTO `owner` (cpf, ownerName, ownerLastName, phoneNumber, address ) VALUES (44444444444,"Amoedo", "Lucas", 61961616161, "Rua dos Desenvolvedores, Alameda dos Cheaters, Numero Anônimo");
