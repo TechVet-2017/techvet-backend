2d425bcf99c2a06cc141afe871a61ac0e9cda7c4
Flavio Costa <skingk.f@gmail.com>
2017-04-01 17:35:04 -0300

    Refactor database migrate attributes names
    Related to issue #4
    
    Signed-off-by: Flavio Costa <skingk.f@gmail.com>
    Signed-off-by: leomeister <leomeister22@gmail.com>

--- src/main/resources/db/migration/V1.2.1__Create_patient_table.sql
+++ src/main/resources/db/migration/V1.2.1__Create_patient_table.sql
@@ -1,14 +1,14 @@
 DROP TABLE IF EXISTS `patient`;
 CREATE TABLE `patient`(
-	`patientId` int NOT NULL,
-	`patientName` varchar(55) NOT NULL,
-	`species` varchar(55) NOT NULL,
-	`race` varchar(55) NOT NULL,
+	`id_patient` int NOT NULL,
+	`name_patient` varchar(50) NOT NULL,
+	`specie` char(1) NOT NULL,
+	`breed` varchar(20) NOT NULL,
 	`size` char(1) NOT NULL,
 	`gender` char(1) NOT NULL,
-	`birthDate` DATE NOT NULL,
-	`furCharacteristics` varchar(55) NOT NULL,
+	`birthday` DATE NOT NULL,
+	`coat` varchar(10) NOT NULL,
 	`weight` float NOT NULL,	
-	PRIMARY KEY(`patientId`)
+	PRIMARY KEY(`id_patient`)
 );
 	
