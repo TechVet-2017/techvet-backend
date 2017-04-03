03eaec2dabe5b7cc3ec311a42f6df8da82707435
Adailson Santos <adailson2@gmail.com>
2017-04-03 12:23:47 -0300

    Separated column address in owner's migration
    Replaced column address for zip_code, district, public_place and adress_number columns
    
    Related to #6
    
    Signed-off-by: Adailson Santos <adailson2@gmail.com>
    Signed-off-by: João Paulo Ferreira <iamferreirajp@gmail.com>

--- src/main/resources/db/migration/V1.1.1__Create_owner_table.sql
+++ src/main/resources/db/migration/V1.1.1__Create_owner_table.sql
@@ -1,10 +1,13 @@
 DROP TABLE IF EXISTS `owner`;
 CREATE TABLE `owner`(
-	`ownerId` int NOT NULL AUTO_INCREMENT,
-	`cpf` bigint(11) NOT NULL UNIQUE,
-	`ownerName` varchar(50) NOT NULL,
-	`ownerLastName` varchar(50) NOT NULL,
-	`phoneNumber` bigint(20) NOT NULL,
- 	`address` varchar(200) NOT NULL,
- 	PRIMARY KEY(`ownerId`)
+	`id_owner` bigint(10) NOT NULL AUTO_INCREMENT,
+	`cpf` bigint(11) NOT NULL,
+	`owner_name` varchar(50) NOT NULL,
+	`owner_last_name` varchar(50) NOT NULL,
+	`phone_number` bigint(20) NOT NULL,
+ 	`zip_code` bigint(8) NULL,
+ 	`district` varchar(50) NOT NULL,
+ 	`public_place` varchar(50) NOT NULL,
+ 	`address_number` bigint(10) NOT NULL,
+ 	PRIMARY KEY(`id_owner`)
 );
--- src/main/resources/db/migration/V1.1.2__Seed_owner_table.sql
+++ src/main/resources/db/migration/V1.1.2__Seed_owner_table.sql
@@ -1,4 +1,4 @@
-INSERT INTO `owner` (cpf, ownerName, ownerLastName, phoneNumber, address ) VALUES (11111111111,"João Paulo", "Ferreira", 62982182377, "Rua das Maravilhas Número 10 Lote");
-INSERT INTO `owner` (cpf, ownerName, ownerLastName, phoneNumber, address ) VALUES (22222222222,"Adalto", "Santos", 61999999999, "Rua das Acácias Número 23 Lote 02");
-INSERT INTO `owner` (cpf, ownerName, ownerLastName, phoneNumber, address ) VALUES (33333333333,"Nicácio", "Estagiário", 61111111111, "CPD da FGA Sala 10 das 10h as 17h");
-INSERT INTO `owner` (cpf, ownerName, ownerLastName, phoneNumber, address ) VALUES (44444444444,"Amoedo", "Lucas", 61961616161, "Rua dos Desenvolvedores, Alameda dos Cheaters, Numero Anônimo");
+INSERT INTO `owner` (cpf, owner_name, owner_last_name, phone_number, zip_code, district, public_place, address_number) VALUES (11111111111, "João Paulo", "Ferreira", 62982182377, 72992864, "Gama","Avenida", 20);
+INSERT INTO `owner` (cpf, owner_name, owner_last_name, phone_number, zip_code, district, public_place, address_number) VALUES (22222222222, "Adalto", "Santos", 61999999999, 72212823, "Recanto","Setor", 8);
+INSERT INTO `owner` (cpf, owner_name, owner_last_name, phone_number, zip_code, district, public_place, address_number) VALUES (33333333333, "Nicácio", "Estagiário", 61111111111, 72639121, "Gama","Setor", 35);
+INSERT INTO `owner` (cpf, owner_name, owner_last_name, phone_number, zip_code, district, public_place, address_number) VALUES (44444444444, "Amoedo", "Lucas", 61961616161, 72661289, "Gama","Quadra", 23);
