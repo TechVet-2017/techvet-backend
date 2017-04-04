3ad7c0ba268e832822ec5d84f05a1614310dbf63
Adailson Santos <adailson2@gmail.com>
2017-04-04 09:38:56 -0300

    Added comments for Owner and OwnerDAO classes
    Related to #6
    Signed-off-by: Adailson Santos <adailson2@gmail.com>
    Signed-off-by: João Paulo Ferreira <iamjpiamferreirajp@gmail.com>

--- src/main/java/br/com/bovdog/dao/OwnerDAO.java
+++ src/main/java/br/com/bovdog/dao/OwnerDAO.java
@@ -5,17 +5,21 @@ import java.util.List;
 
 import br.com.bovdog.bean.Owner;
 
+//create class Owner DAO for database communication.
 public class OwnerDAO {
 	private final String URL = "jdbc:mysql://localhost/techvet?useSSL=false&serverTimezone=UTC";
 	private final String USER = "root";
 	private final String PASSWORD = "root";
 	
+	// create method for creation of owners in database.
 	public void createOwner(Owner owner){
 		Connection connection = null;
 		PreparedStatement preparedStatement = null;
 		String sqlStatement = "INSERT INTO `owner` (id_owner, cpf, owner_name, owner_last_name, phone_number, zip_code, "
 				+ "district, public_place, address_number) "
 				+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		
+		// treatment to ensure the successful insert of the operation.
 		try{
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			connection = DriverManager.getConnection(URL, USER, PASSWORD);
@@ -30,10 +34,16 @@ public class OwnerDAO {
 			preparedStatement.setString(8, owner.getPublicPlace());
 			preparedStatement.setLong(9, owner.getAddressNumber());
 			preparedStatement.executeUpdate();
+			
+		// return error if caught SQL exception.
 		} catch(SQLException exception) {
 			exception.printStackTrace();
+		
+		// return error if caught class exception.	
 		} catch(ClassNotFoundException exception) {
 			exception.printStackTrace();
+		
+		// close SQL statement and database connection. 
 		} finally {
 			try { 
 				if(preparedStatement != null){
@@ -41,15 +51,18 @@ public class OwnerDAO {
 				} else {
 					// Nothing to do
 				}
+			// return error if caught SQL exception.
 			} catch(SQLException exception){
 					exception.printStackTrace();
 			}
+			
 			try { 
 				if(connection != null){
 					connection.close(); 
 				} else {
 					// Nothing to do
 				}
+			// return error if caught SQL exception.
 			} catch(SQLException exception){
 				exception.printStackTrace();
 			}
@@ -62,7 +75,8 @@ public class OwnerDAO {
 		String sqlStatement = "UPDATE owner SET owner_name = ?, owner_last_name = ?, phone_number = ?, zip_code = ?, "
 				+ "district = ?, public_place = ?, address_number = ?  "
 				+ "WHERE cpf = ? ";
-	
+		
+		// treatment to ensure the successful update of the operation.
 		try{
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			connection = DriverManager.getConnection(URL, USER, PASSWORD);
@@ -75,26 +89,37 @@ public class OwnerDAO {
 			preparedStatement.setString(6, owner.getPublicPlace());
 			preparedStatement.setLong(7, owner.getAddressNumber());
 			preparedStatement.executeUpdate();
+		
+		// return error if caught SQL exception.	
 		} catch(SQLException exception) {
 			exception.printStackTrace();
+		
+		// return error if caught class exception.	
 		} catch(ClassNotFoundException exception) {
 			exception.printStackTrace();
+		
+		// close SQL statement and database connection.  
 		} finally {
+			
 			try { 
 				if(preparedStatement != null){
 					preparedStatement.close(); 
 				} else {
 					// Nothing to do
 				}
+				
+			// return error if caught SQL exception.	
 			} catch(SQLException exception){
 					exception.printStackTrace();
 			}
+			
 			try { 
 				if(connection != null){
 					connection.close(); 
 				} else {
 					// Nothing to do
 				}
+			// return error if caught SQL exception.	
 			} catch(SQLException exception){
 				exception.printStackTrace();
 			}
@@ -105,17 +130,24 @@ public class OwnerDAO {
 		Connection connection = null;
 		PreparedStatement preparedStatement = null;
 		String sqlStatement = "DELETE FROM owner WHERE cpf = ?";
-	
+		
+		// treatment to ensure the successful delete of the operation.
 		try{
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			connection = DriverManager.getConnection(URL, USER, PASSWORD);
 			preparedStatement = connection.prepareStatement(sqlStatement);
 			preparedStatement.setLong(1, cpf);
 			preparedStatement.executeUpdate();
+		
+		// return error if caught SQL exception.	
 		} catch(SQLException exception) {
 			exception.printStackTrace();
+		
+		// return error if caught class exception.	
 		} catch(ClassNotFoundException exception) {
 			exception.printStackTrace();
+	
+		// close SQL statement and database connection.  		
 		} finally {
 			try { 
 				if(preparedStatement != null){
@@ -123,6 +155,7 @@ public class OwnerDAO {
 				} else {
 					// Nothing to do
 				}
+			// return error if caught SQL exception.	
 			} catch(SQLException exception){
 					exception.printStackTrace();
 			}
@@ -132,6 +165,7 @@ public class OwnerDAO {
 				} else {
 					// Nothing to do
 				}
+			// return error if caught SQL exception.		
 			} catch(SQLException exception){
 				exception.printStackTrace();
 			}
@@ -143,16 +177,23 @@ public class OwnerDAO {
 		PreparedStatement preparedStatement = null;
 		String sqlStatement = "DELETE FROM owner WHERE id_owner = ?";
 	
+		// treatment to ensure the successful delete of the operation.
 		try{
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			connection = DriverManager.getConnection(URL, USER, PASSWORD);
 			preparedStatement = connection.prepareStatement(sqlStatement);
 			preparedStatement.setLong(1, ownerId);
 			preparedStatement.executeUpdate();
+		
+		// return error if caught SQL exception.		
 		} catch(SQLException exception) {
 			exception.printStackTrace();
+			
+		// return error if caught class exception.
 		} catch(ClassNotFoundException exception) {
 			exception.printStackTrace();
+			
+		// close SQL statement and database connection. 
 		} finally {
 			try { 
 				if(preparedStatement != null){
@@ -160,6 +201,7 @@ public class OwnerDAO {
 				} else {
 					// Nothing to do
 				}
+			// return error if caught SQL exception.
 			} catch(SQLException exception){
 					exception.printStackTrace();
 			}
@@ -169,17 +211,21 @@ public class OwnerDAO {
 				} else {
 					// Nothing to do
 				}
+			// return error if caught SQL exception.
 			} catch(SQLException exception){
 				exception.printStackTrace();
 			}
 		}
 	}
 	
+	// create method to select a specific owner from table by name.
 	public List<Owner> findOwnerByName(String insertedName){
 		List<Owner> owners = new ArrayList<Owner>();
 		Connection connection = null;
 		PreparedStatement preparedStatement = null;
 		String sqlStatement = "SELECT * FROM owner WHERE owner_name like CONCAT('%', ? ,'%')";
+		
+		// treatment for existence of desired owner name.
 		try{
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			connection = DriverManager.getConnection(URL, USER, PASSWORD);
@@ -187,6 +233,10 @@ public class OwnerDAO {
 			preparedStatement.setString(1, insertedName);
 			ResultSet results = preparedStatement.executeQuery();
 			
+			/*
+			 * if any results exist in one list, it increments Owner attributes 
+			 * in the next list.
+			 */
 			while(results.next()){
 				Owner owner = new Owner();
 				owner.setOwnerId(results.getInt("id_owner"));
@@ -200,8 +250,12 @@ public class OwnerDAO {
 				owner.setAddressNumber(results.getLong("address_number"));
 				owners.add(owner);
 			}
+			
+		// return error if caught SQL exception
 		} catch(SQLException exception) {
 			exception.printStackTrace();
+			
+		// return error if caught class exception
 		} catch(ClassNotFoundException exception) {
 			exception.printStackTrace();
 		} finally {
@@ -211,6 +265,7 @@ public class OwnerDAO {
 				} else {
 					// Nothing to do
 				}
+			// return error if caught SQL exception
 			} catch(SQLException exception){
 					exception.printStackTrace();
 			}
@@ -220,17 +275,22 @@ public class OwnerDAO {
 				} else {
 					// Nothing to do
 				}
+			// return error if caught SQL exception
 			} catch(SQLException exception){
 				exception.printStackTrace();
 			}
 		}
 		return owners;
 	}
+	
+	// create method to select a specific owner from table by cpf.
 	public List<Owner> findOwnerByCpf(Long insertedCpf){
 		List<Owner> owners = new ArrayList<Owner>();
 		Connection connection = null;
 		PreparedStatement preparedStatement = null;
 		String sqlStatement = "SELECT * FROM owner WHERE cpf like CONCAT('%',?,'%')";
+		
+		// treatment for existence of desired owner cpf.
 		try{
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			connection = DriverManager.getConnection(URL, USER, PASSWORD);
@@ -238,6 +298,10 @@ public class OwnerDAO {
 			preparedStatement.setLong(1, insertedCpf);
 			ResultSet results = preparedStatement.executeQuery();
 			
+			/*
+			 * if any results exist in one list, it increments Owner attributes 
+			 * in the next list.
+			 */
 			while(results.next()){
 				Owner owner = new Owner();
 				owner.setOwnerId(results.getInt("id_owner"));
@@ -251,10 +315,16 @@ public class OwnerDAO {
 				owner.setAddressNumber(results.getLong("address_number"));
 				owners.add(owner);
 			}
+			
+		// return error if caught SQL exception
 		} catch(SQLException exception) {
 			exception.printStackTrace();
+			
+		// return error if caught class exception
 		} catch(ClassNotFoundException exception) {
 			exception.printStackTrace();
+			
+		// close SQL statement and database connection. 
 		} finally {
 			try { 
 				if(preparedStatement != null){
@@ -262,6 +332,7 @@ public class OwnerDAO {
 				} else {
 					// Nothing to do
 				}
+			// return error if caught SQL exception	
 			} catch(SQLException exception){
 					exception.printStackTrace();
 			}
@@ -271,18 +342,23 @@ public class OwnerDAO {
 				} else {
 					// Nothing to do
 				}
+			// return error if caught SQL exception
 			} catch(SQLException exception){
 				exception.printStackTrace();
 			}
 		}
+		// returns wanted owners.
 		return owners;
 	}	
 	
+	// create method to select a specific owner from table by phone number.
 	public List<Owner> findOwnerByPhoneNumber(Long insertedPhoneNumber){
 		List<Owner> owners = new ArrayList<Owner>();
 		Connection connection = null;
 		PreparedStatement preparedStatement = null;
 		String sqlStatement = "SELECT * FROM owner WHERE phone_number like CONCAT('%',?,'%')";
+		
+		// treatment for existence of desired owner phone number.
 		try{
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			connection = DriverManager.getConnection(URL, USER, PASSWORD);
@@ -290,6 +366,10 @@ public class OwnerDAO {
 			preparedStatement.setLong(1, insertedPhoneNumber);
 			ResultSet results = preparedStatement.executeQuery();
 			
+			/*
+			 * if any results exist in one list, it increments Owner attributes 
+			 * in the next list.
+			 */
 			while(results.next()){
 				Owner owner = new Owner();
 				owner.setOwnerId(results.getInt("id_owner"));
@@ -303,10 +383,16 @@ public class OwnerDAO {
 				owner.setAddressNumber(results.getLong("address_number"));	
 				owners.add(owner);
 			}
+			
+		// return error if caught SQL exception
 		} catch(SQLException exception) {
 			exception.printStackTrace();
+			
+		// return error if caught class exception
 		} catch(ClassNotFoundException exception) {
 			exception.printStackTrace();
+			
+		// close SQL statement and database connection.
 		} finally {
 			try { 
 				if(preparedStatement != null){
@@ -314,6 +400,7 @@ public class OwnerDAO {
 				} else {
 					// Nothing to do
 				}
+			// return error if caught SQL exception
 			} catch(SQLException exception){
 					exception.printStackTrace();
 			}
@@ -323,24 +410,33 @@ public class OwnerDAO {
 				} else {
 					// Nothing to do
 				}
+			// return error if caught SQL exception
 			} catch(SQLException exception){
 				exception.printStackTrace();
 			}
 		}
+		// returns wanted owners.
 		return owners;	
 	}
 	
+	// create method getAllPatients to return list of Owners.
 	public List<Owner> getAllOwners(){
 		List<Owner> owners = new ArrayList<Owner>();
 		Connection connection = null;
 		PreparedStatement preparedStatement = null;
 		String sqlStatement = "SELECT * FROM owner";
+		
+		// treatment for increment in Owner list.
 		try{
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			connection = DriverManager.getConnection(URL, USER, PASSWORD);
 			preparedStatement = connection.prepareStatement(sqlStatement);
 			ResultSet results = preparedStatement.executeQuery();
 			
+			/*
+			 * if any results exist in one list, it increments Owner attributes 
+			 * in the next list.
+			 */
 			while(results.next()){
 				Owner owner = new Owner();
 				owner.setOwnerId(results.getInt("id_owner"));
@@ -354,17 +450,25 @@ public class OwnerDAO {
 				owner.setAddressNumber(results.getLong("address_number"));
 				owners.add(owner);
 			}
+			
+		// return error if caught SQL exception.
 		} catch(SQLException exception) {
 			exception.printStackTrace();
+		
+		// return error if caught class exception.
 		} catch(ClassNotFoundException exception) {
 			exception.printStackTrace();
+			
+		// close SQL statement and database connection. 
 		} finally {
 			try { 
+				
 				if(preparedStatement != null){
 					preparedStatement.close(); 
 				} else {
 					// Nothing to do
 				}
+			// return error if caught SQL exception.
 			} catch(SQLException exception){
 					exception.printStackTrace();
 			}
@@ -374,10 +478,13 @@ public class OwnerDAO {
 				} else {
 					// Nothing to do
 				}
+			// return error if caught SQL exception.
 			} catch(SQLException exception){
 				exception.printStackTrace();
 			}
 		}
+		
+		// return Owner list.
 		return owners;
 	}
 
