a437bb8a33abe87e8c021de7873a5576ff4846c4
antoniocoj <_antonio_@hotmail.com>
2017-04-03 21:33:30 -0300

    Create UserService GetAllUsers
    Related to issue #11
    
    Signed-off-by: antoniocoj <_antonio_@hotmail.com>
    Signed-off-by: luizguilherme5 <do.guilherme@hotmail.com>

---
+++ src/main/java/br/com/bovdog/service/UserService.java
@@ -0,0 +1,27 @@
+package br.com.bovdog.service;
+
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import br.com.bovdog.bean.User;
+import br.com.bovdog.dao.UserDAO;
+
+@Path("/UserService")
+
+public class UserService {
+	
+	@GET
+	@Path("/users")
+	@Produces("applicaion/json")
+	public List<User> getAllUsers(){
+		UserDAO dao = new UserDAO();
+		return dao.getAllUsers();
+	}
+}
