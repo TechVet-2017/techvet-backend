65b6ccbc4777c86f6a838830ecfae12a3f0f5be4
leomeister <leomeister22@gmail.com>
2017-04-04 00:51:43 -0300

    Create new patient on patient seed
    Related to issue #4
    
    Signed-off-by: leomeister <leomeister22@gmail.com>
    Signed-off-by: Flavio Costa <skingk.f@gmail.com>
    Signed-off-by: Lucas Amoedo <lucas_advc@outlook.com>

--- src/main/resources/db/migration/V1.2.2__Create_patient_seed.sql
+++ src/main/resources/db/migration/V1.2.2__Create_patient_seed.sql
@@ -1,2 +1,5 @@
 INSERT INTO patient (name_patient, specie, breed, size, gender, birthday, coat)
 VALUES ('Astolfo', 'C', 'Bengal', 'G', 'M', '20170403', 'Black');
+
+INSERT INTO patient (name_patient, specie, breed, size, gender, birthday, coat)
+VALUES ('Rodolfo', 'D', 'Shitzu', 'P', 'M', '20170210', 'Gray');
