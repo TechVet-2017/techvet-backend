7b91f4dbc61e9d0263f7af79455596b6fda227f5
Lucas Amoêdo <lucas_advc@outlook.com>
2017-04-06 12:58:39 -0300

    Add Hibernate configuration
    Related to issue #12
    
    Signed-off-by: Lucas Amoêdo <lucas_advc@outlook.com>

--- src/main/java/br/com/bovdog/bean/ClinicalRecord.java
+++ src/main/java/br/com/bovdog/bean/ClinicalRecord.java
@@ -1,8 +1,15 @@
 package br.com.bovdog.bean;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+
+@Entity
 public class ClinicalRecord {
-	
-	private int c_protocol;
+
+  @Id
+  @GeneratedValue
+  private int clinicalRecordId;
 	private int patientId;
 	
 	private String anamnesis;
@@ -15,92 +22,110 @@ public class ClinicalRecord {
 	private String mucosasApparent;
 	private float patientRespiratoryRate;
 	private float patientHeartRate;
+	private float patientWeight; 
+
+  public int getClinicalRecordId() {
+    return this.clinicalRecordId;
+  }
+
+  public void setClinicalRecordId(int clinicalRecordId) {
+    this.clinicalRecordId = clinicalRecordId;
+  }
 	
-	
-	public int getC_protocol() {
-		return c_protocol;
-	}
-	public void setC_protocol(int c_protocol) {
-		this.c_protocol = c_protocol;
-	}
 	public int getPatientId() {
-		return patientId;
+		return this.patientId;
 	}
+
 	public void setPatientId(int patientId) {
 		this.patientId = patientId;
 	}
+
 	public String getAnamnesis() {
-		return anamnesis;
+		return this.anamnesis;
 	}
+
 	public void setAnamnesis(String anamnesis) {
 		this.anamnesis = anamnesis;
 	}
+
 	public String getVeterinarian() {
-		return veterinarian;
+		return this.veterinarian;
 	}
+  
 	public void setVeterinarian(String veterinarian) {
 		this.veterinarian = veterinarian;
 	}
+
 	public String getClinical_history() {
-		return clinical_history;
+		return this.clinical_history;
 	}
+
 	public void setClinical_history(String clinical_history) {
 		this.clinical_history = clinical_history;
 	}
+
 	public String getDiagnosis() {
-		return diagnosis;
+		return this.diagnosis;
 	}
+
 	public void setDiagnosis(String diagnosis) {
 		this.diagnosis = diagnosis;
 	}
+
 	public float getPatientTemperature() {
-		return patientTemperature;
+		return this.patientTemperature;
 	}
+
 	public void setPatientTemperature(float patientTemperature) {
 		this.patientTemperature = patientTemperature;
 	}
+
 	public float getCapillaryFill() {
-		return capillaryFill;
+		return this.capillaryFill;
 	}
+
 	public void setCapillaryFill(float capillaryFill) {
 		this.capillaryFill = capillaryFill;
 	}
+
 	public String getPatientPulse() {
-		return patientPulse;
+		return this.patientPulse;
 	}
+
 	public void setPatientPulse(String patientPulse) {
 		this.patientPulse = patientPulse;
 	}
+
 	public String getMucosasApparent() {
-		return mucosasApparent;
+		return this.mucosasApparent;
 	}
+
 	public void setMucosasApparent(String mucosasApparent) {
 		this.mucosasApparent = mucosasApparent;
 	}
+
 	public float getPatientRespiratoryRate() {
-		return patientRespiratoryRate;
+		return this.patientRespiratoryRate;
 	}
+
 	public void setPatientRespiratoryRate(float patientRespiratoryRate) {
 		this.patientRespiratoryRate = patientRespiratoryRate;
 	}
+
 	public float getPatientHeartRate() {
-		return patientHeartRate;
+		return this.patientHeartRate;
 	}
+
 	public void setPatientHearRate(float patientHearRate) {
 		this.patientHeartRate = patientHearRate;
 	}
+
 	public float getPatientWeight() {
-		return patientWeight;
+		return this.patientWeight;
 	}
+
 	public void setPatientWeight(float patientWeight) {
 		this.patientWeight = patientWeight;
 	}
-	private float patientWeight; 
-	
-	
-	
-
-	
-	
 
 }
