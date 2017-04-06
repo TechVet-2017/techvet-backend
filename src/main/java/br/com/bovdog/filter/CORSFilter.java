445d666e9eb5333bca978cb0bedbdd9665bbd51b
Lucas Amoêdo <lucas_advc@outlook.com>
2017-04-06 12:57:01 -0300

    Create CORSFilter
    Related to issue #9
    
    Signed-off-by: Lucas Amoêdo <lucas_advc@outlook.com>

---
+++ src/main/java/br/com/bovdog/filter/CORSFilter.java
@@ -0,0 +1,18 @@
+package br.com.bovdog.filter;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class CORSFilter implements ContainerResponseFilter {
+
+	public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
+		response.getHeaders().add("Access-Control-Allow-Origin", "*");
+		response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, DELETE");
+	}
+	
+}
