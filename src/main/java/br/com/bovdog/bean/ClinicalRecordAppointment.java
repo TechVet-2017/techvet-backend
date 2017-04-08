bf741f2af796d5c3cd4a1b91a543e5daafcab725
Lucas Amoêdo <lucas_advc@outlook.com>
2017-04-08 10:56:25 -0300

    Create ClinicalRecordAppointment class
    Related to issue #9
    
    Signed-off-by: Lucas Amoêdo <lucas_advc@outlook.com>
    Signed-off-by: Marcelo Oliveira <martins.oliveira.mo@gmail.com>

---
+++ src/main/java/br/com/bovdog/bean/ClinicalRecordAppointment.java
@@ -0,0 +1,113 @@
+package br.com.bovdog.bean;
+
+public class ClinicalRecordAppointment extends ClinicalRecord {
+
+    private String procedure;
+    private String observations;
+    private String exam;
+    private String informations;
+    private String preliminaryMedication;
+    private float medicationDosage;
+    private String medicationFrequency;
+    private String diagnostic; 
+    private String prognostic;
+    private String homeMedicationName;
+    private String homeMedicationDosage;
+    private String homeMedicationFrequency;
+    private String clinicMedicationName;
+    private String clinicMedicationDosage;
+    private String clinicMedicationFrequency;
+
+
+    public String getProcedure() {
+        return this.procedure;
+    }
+    public void setProcedure(String procedure) {
+        this.procedure = procedure;
+    }
+    public String getObservations() {
+        return this.observations;
+    }
+    public void setObservations(String observations) {
+        this.observations = observations;
+    }
+    public String getExam() {
+        return this.exam;
+    }
+    public void setExam(String exam) {
+        this.exam = exam;
+    }
+    public String getInformations() {
+        return this.informations;
+    }
+    public void setInformations(String informations) {
+        this.informations = informations;
+    }
+    public String getPreliminaryMedication() {
+        return this.preliminaryMedication;
+    }
+    public void setPreliminaryMedication(String preliminaryMedication) {
+        this.preliminaryMedication = preliminaryMedication;
+    }
+    public float getMedicationDosage() {
+        return this.medicationDosage;
+    }
+    public void setMedicationDosage(float medicationDosage) {
+        this.medicationDosage = medicationDosage;
+    }
+    public String getMedicationFrequency() {
+        return this.medicationFrequency;
+    }
+    public void setMedicationFrequency(String medicationFrequency) {
+        this.medicationFrequency = medicationFrequency;
+    }
+    public String getDiagnostic() {
+        return this.diagnostic;
+    }
+    public void setDiagnostic(String diagnostic) {
+        this.diagnostic = diagnostic;
+    }
+    public String getPrognostic() {
+        return this.prognostic;
+    }
+    public void setPrognostic(String prognostic) {
+        this.prognostic = prognostic;
+    }
+    public String getHomeMedicationName() {
+        return this.homeMedicationName;
+    }
+    public void setHomeMedicationName(String homeMedicationName) {
+        this.homeMedicationName = homeMedicationName;
+    }
+    public String getHomeMedicationDosage() {
+        return this.homeMedicationDosage;
+    }
+    public void setHomeMedicationDosage(String homeMedicationDosage) {
+        this.homeMedicationDosage = homeMedicationDosage;
+    }
+    public String getHomeMedicationFrequency() {
+        return this.homeMedicationFrequency;
+    }
+    public void setHomeMedicationFrequency(String homeMedicationFrequency) {
+        this.homeMedicationFrequency = homeMedicationFrequency;
+    }
+    public String getClinicMedicationName() {
+        return this.clinicMedicationName;
+    }
+    public void setClinicMedicationName(String clinicMedicationName) {
+        this.clinicMedicationName = clinicMedicationName;
+    }
+    public String getClinicMedicationDosage() {
+        return this.clinicMedicationDosage;
+    }
+    public void setClinicMedicationDosage(String clinicMedicationDosage) {
+        this.clinicMedicationDosage = clinicMedicationDosage;
+    }
+    public String getClinicMedicationFrequency() {
+        return this.clinicMedicationFrequency;
+    }
+    public void setClinicMedicationFrequency(String clinicMedicationFrequency) {
+        this.clinicMedicationFrequency = clinicMedicationFrequency;
+    }
+
+}
