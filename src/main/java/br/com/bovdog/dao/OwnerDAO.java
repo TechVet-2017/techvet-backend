cffe94f38aa71b1281adf1e413b4a8e4ebf1897c
João Paulo Ferreira <iamferreirajp@gmail.com>
2017-04-09 21:20:33 -0300

    Logging the OwnerDAO methods Related to issue #15 Related to issue #6
    Signed-off-by: João Paulo Ferreira <iamferreirajp@gmail.com>
    Signed-off-by: Adailson Santos <adailson2@gmail.com>

--- src/main/java/br/com/bovdog/dao/OwnerDAO.java
+++ src/main/java/br/com/bovdog/dao/OwnerDAO.java
@@ -9,20 +9,25 @@ import javax.persistence.Persistence;
 
 import br.com.bovdog.bean.ClinicalRecord;
 import br.com.bovdog.bean.Owner;
-
+import org.apache.log4j.Logger;
 //create class Owner DAO for database communication.
 public class OwnerDAO {
+	
+	// Initializing the log service
+	final static Logger logger = Logger.getLogger(OwnerDAO.class);
 	
 	private EntityManager entityManager = null;
 	
 	public OwnerDAO(){
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("techvet-unit");
 		this.entityManager = factory.createEntityManager();
+		logger.debug("Constructor method with entity manager object = " + factory);
 	}
 
 	// Find the owner in database using his id.
 	public Owner getOwnerById(int id){
 	    Owner owner = entityManager.find(Owner.class, id);
+	    logger.debug("getOwnerById method with id "+ id +" and object owner = " + owner);
 	    return owner;
 	}
 	
@@ -31,9 +36,11 @@ public class OwnerDAO {
 		try{
 			entityManager.getTransaction().begin();
 		    entityManager.persist(owner);
+		    logger.debug("createOwner method with object owner = " + owner);
 		    entityManager.getTransaction().commit();			
 		} catch(Exception exception) {
 			exception.printStackTrace();
+			logger.fatal("catch statement on createOwner with exception = " + exception);
 			entityManager.getTransaction().rollback();
 		}
 	}
@@ -43,9 +50,11 @@ public class OwnerDAO {
 		try{
 			entityManager.getTransaction().begin();
 		    entityManager.merge(owner);
+		    logger.debug("updateOwner method with object owner = " + owner);
 		    entityManager.getTransaction().commit();			
 		} catch(Exception exception) {
 			exception.printStackTrace();
+			logger.fatal("catch statement on updateOwner with exception = " + exception);
 			entityManager.getTransaction().rollback();
 		}
 	}
@@ -55,10 +64,12 @@ public class OwnerDAO {
 	    Owner owner = getOwnerById(id);
 	    try {
 	      entityManager.getTransaction().begin();
+	      logger.debug("deleteOwner with id ("+ id +") method with object owner = " + owner);
 	      entityManager.remove(owner);
 	      entityManager.getTransaction().commit();
 	    } catch(Exception exception) {
 	      exception.printStackTrace();
+	      logger.fatal("catch statement on deleteOwner with exception = " + exception);
 	      entityManager.getTransaction().rollback();
 	    }
 	}
@@ -67,6 +78,7 @@ public class OwnerDAO {
 	public List<Owner> findOwnerByCpf(String insertedCpf){
 	    List<Owner> owners = entityManager.createQuery("SELECT t FROM Owner t WHERE t.cpf LIKE :cpf")
 	    		.setParameter("cpf", "%"+insertedCpf+"%").getResultList();
+	    logger.debug("findOwnerByCpf method with cpf = ("+ insertedCpf +") and object owners = " + owners);
 		return owners;	
 	}	
 	
@@ -74,6 +86,7 @@ public class OwnerDAO {
 	public List<Owner> findOwnerByName(String insertedName){
 	    List<Owner> owners = entityManager.createQuery("SELECT t FROM Owner t WHERE t.ownerName LIKE :name")
 	    		.setParameter("name","%"+insertedName+"%").getResultList();
+	    logger.debug("findOwnerByName method with name = ("+ insertedName +") and object owners = " + owners);
 		return owners;	
 	}	
 	
@@ -81,6 +94,7 @@ public class OwnerDAO {
 	public List<Owner> findOwnerByPhoneNumber(String insertedPhoneNumber){
 	    List<Owner> owners = entityManager.createQuery("SELECT t FROM Owner t WHERE t.phoneNumber LIKE :phone")
 	    		.setParameter("phone", "%"+insertedPhoneNumber+"%").getResultList();
+	    logger.debug("findOwnerByPhoneNumber method with phone = ("+ insertedPhoneNumber +") and object owners = " + owners);
 		return owners;	
 	}
 	
@@ -88,6 +102,7 @@ public class OwnerDAO {
 	public List<Owner> getAllOwners(){
 	    List<Owner> owners = new ArrayList<Owner>();
 	    owners = entityManager.createQuery("FROM " + Owner.class.getName()).getResultList();
+	    logger.debug("getAllOwners method with and object owners = " + owners);
 	    return owners;
 	}
 
