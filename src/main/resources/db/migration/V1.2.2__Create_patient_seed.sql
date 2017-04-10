30d2b8f0321d9e9a0a08f23fb5044974f69bca2c
leomeister <leomeister22@gmail.com>
2017-04-10 09:52:32 -0300

    Fixing more merge problems
    Related to issue #4
    
    Signed-off-by: leomeister <leomeister22@gmail.com>
    Signed-off-by: Flavio Costa <skingk.f@gmail.com>

--- src/main/resources/db/migration/V1.2.2__Create_patient_seed.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO patient (name_patient, specie, breed, size, gender, birthday, coat)
-VALUES ('Astolfo', 'C', 'Bengal', 'G', 'M', '20170403', 'Black');
-
-INSERT INTO patient (name_patient, specie, breed, size, gender, birthday, coat)
-VALUES ('Rodolfo', 'D', 'Shitzu', 'P', 'M', '20170210', 'Gray');
