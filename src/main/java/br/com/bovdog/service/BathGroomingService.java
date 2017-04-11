e0c0e52480e4211c98e92b087231344bbfc30da2
leomeister <leomeister22@gmail.com>
2017-04-10 21:37:09 -0300

    Update HTTP requests in Bath and Grooming service
    Related to issue #24
    
    Signed-off-by: leomeister <leomeister22@gmail.com>
    Signed-off-by: Lucas Amoêdo <lucas_advc@outlook.com>

--- src/main/java/br/com/bovdog/service/BathGroomingService.java
+++ src/main/java/br/com/bovdog/service/BathGroomingService.java
@@ -3,10 +3,13 @@ package br.com.bovdog.service;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import br.com.bovdog.bean.BathGrooming;
@@ -15,7 +18,7 @@ import br.com.bovdog.dao.BathGroomingDAO;
 import br.com.bovdog.dao.OwnerDAO;
 
 
-@Path("/BathGroomingService")
+@Path("/bathAndGrooming")
 public class BathGroomingService {
 	
 	private BathGroomingDAO dao = null;
@@ -26,18 +29,18 @@ public class BathGroomingService {
 	}
 	
 	// creating the method to find a specific object.
-	@POST
-	@Path("/getById")
+	@GET
+	@Path("/{id:[0-9]+}")
 	@Consumes("application/x-www-form-urlencoded")
 	@Produces("application/json")
-	public BathGrooming getBathGroomingById(@FormParam("id") int id) {
+	public BathGrooming getBathGroomingById(@PathParam("id") int id) {
 		BathGroomingDAO dao = new BathGroomingDAO();
 	    return dao.getBathGroomingById(id);
 	}
 
 	// creating the method to return all BathGroomings objects.
 	@GET
-	@Path("/getAll")
+	@Path("/")
 	@Produces("application/json")
 	public List<BathGrooming> getAllBathGroomings() {
 		BathGroomingDAO dao = new BathGroomingDAO();
@@ -45,22 +48,22 @@ public class BathGroomingService {
 	}
 	
 	// creating the method to delete a specific object.
-	@POST
-	@Path("/delete")
+	@DELETE
+	@Path("/{id:[0-9]+}")
 	@Consumes("application/x-www-form-urlencoded")
 	@Produces("application/json")
-	public List<BathGrooming> deleteBathGroomingById(@FormParam("id") int id) {
+	public List<BathGrooming> deleteBathGroomingById(@PathParam("id") int id) {
 		BathGroomingDAO dao = new BathGroomingDAO();
 		dao.deleteBathGrooming(id);
 		return dao.getAllBathGroomings();
 	}
 	  
 	// creating the method to update an object.
-	@POST
-	@Path("/update")
+	@PUT
+	@Path("//{id:[0-9]+}")
 	@Consumes("application/x-www-form-urlencoded")
-	public void updateBathGrooming(@FormParam(value = "idBathGrooming") int idBathGrooming ,
-									@FormParam(value = "serviceBathGrooming") String serviceBathGrooming){
+	public void updateBathGrooming(@PathParam("id") int idBathGrooming ,
+									@FormParam("serviceBathGrooming") String serviceBathGrooming){
 			
 		BathGrooming bathGrooming = new BathGrooming();
 		bathGrooming.setId(idBathGrooming);
