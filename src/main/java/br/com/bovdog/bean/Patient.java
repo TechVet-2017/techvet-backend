54c7f9726ed4247d30ff572c5e9ac04fb897d65d
Varley <varleysilva@me.com>
2017-04-16 01:05:59 -0300

    Adjust methods and variables of species with db and frontend
    Related to issue #4
    
    Signed-off-by: Flavio Costa <skingk.f@gmail.com>
    Signed-off-by: Antonio Carvalho <_antonio_@hotmail.com>
    Signed-off-by: Lucas Amoêdo <lucas_advc@outlook.com>
    Signed-off-by: Varley <varleysilva@me.com>

--- src/main/java/br/com/bovdog/bean/Patient.java
+++ src/main/java/br/com/bovdog/bean/Patient.java
@@ -11,17 +11,17 @@ import javax.persistence.Id;
 // create Patient object.
 @Entity
 public class Patient {
-	
+
 	// notation to identify primary key and auto increment the same
-	@Id 
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	
+
 	// notation to set not null columns attributes
 	@Column(nullable = false)
 	private String patientName;
 	@Column(nullable = false)
-	private String specie;
+	private String species;
 	@Column(nullable = false)
 	private String breed;
 	private char size;
@@ -30,55 +30,70 @@ public class Patient {
 	private Date birthday;
 	@Column(nullable = false)
 	private String coat;
-	
+
 	// getters and setters for Patients attributes.
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getPatientName() {
 		return patientName;
 	}
+
 	public void setPatientName(String patientName) {
 		this.patientName = patientName;
 	}
-	public String getSpecie() {
-		return specie;
+
+	public String getSpecies() {
+		return species;
 	}
-	public void setSpecie(String specie) {
-		this.specie = specie;
+
+	public void setSpecies(String species) {
+		this.species = species;
 	}
+
 	public String getBreed() {
 		return breed;
 	}
+
 	public void setBreed(String breed) {
 		this.breed = breed;
 	}
+
 	public char getSize() {
 		return size;
 	}
+
 	public void setSize(char size) {
 		this.size = size;
 	}
+
 	public char getGender() {
 		return gender;
 	}
+
 	public void setGender(char gender) {
 		this.gender = gender;
 	}
+
 	public Date getBirthday() {
 		return birthday;
 	}
+
 	public void setBirthday(Date birthday) {
 		this.birthday = birthday;
 	}
+
 	public String getCoat() {
 		return coat;
 	}
+
 	public void setCoat(String coat) {
 		this.coat = coat;
 	}
-	
+
 }
