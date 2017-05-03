48505725afa910757e429ac16bcfe8554e4c509f
Lucas Amoêdo <lucas_advc@outlook.com>
2017-05-03 15:25:56 -0300

    Implement generic method for Building queries
    Related to issue #4
    
    Signed-off-by: antoniocoj <_antonio_@hotmail.com>
    Signed-off-by: Lucas Amoêdo <lucas_advc@outlook.com>

--- src/main/java/br/com/bovdog/dao/OwnerDAO.java
+++ src/main/java/br/com/bovdog/dao/OwnerDAO.java
@@ -2,17 +2,11 @@ package br.com.bovdog.dao;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.ParameterExpression;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 import javax.ws.rs.core.MultivaluedMap;
 
 import br.com.bovdog.bean.Owner;
@@ -89,48 +83,7 @@ public class OwnerDAO {
 	// Return all owners that exist in database
 	public List<Owner> getAllOwners(MultivaluedMap<String, String> queryParameters) {
 	    List<Owner> owners = new ArrayList<Owner>();
-	    List<Predicate> criteria = new ArrayList<Predicate>();
-	    
-	    CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-	    CriteriaQuery<Owner> query = criteriaBuilder.createQuery(Owner.class);
-	    Root<Owner> range = query.from(Owner.class);
-	    
-	    String sort = null;
-	    String order = null;
-	    
-	    if(queryParameters.containsKey("_sort")) {
-	    	sort = queryParameters.getFirst("_sort");
-	    } else {
-	    	sort = "id";
-	    }
-	    if(queryParameters.containsKey("_order")) {
-	    	order = queryParameters.getFirst("_order");
-	    } else {
-	    	order = "asc";
-	    }
-	    
-	    for (Map.Entry<String, List<String>> entry : queryParameters.entrySet()){
-	    	if (entry.getKey().charAt(0) != '_') {
-				ParameterExpression<String> parameter = criteriaBuilder.parameter(String.class, entry.getKey()); 
-				criteria.add(criteriaBuilder.like(range.get(parameter.getName()), parameter));
-			}
-	    }
-	    query.select(range);
-	    if (criteria.size() > 0) {
-    		query.where(criteria.toArray(new Predicate[0]));
-	    }
-	    if (order.equalsIgnoreCase("desc")) {
-	    	query.orderBy(criteriaBuilder.desc(range.get(sort)));
-	    } else {
-	    	query.orderBy(criteriaBuilder.asc(range.get(sort)));
-	    }
-	    TypedQuery<Owner> typedQuery = entityManager.createQuery(query);
-	    for (Map.Entry<String, List<String>> entry : queryParameters.entrySet()) {
-	    	if (entry.getKey().charAt(0) != '_') {
-	    		typedQuery.setParameter(entry.getKey(), "%"+entry.getValue().get(0)+"%");
-	    	}
-	    	
-	    }
+	    TypedQuery<Owner> typedQuery = QueryBuilder.buildQuery(queryParameters, entityManager, Owner.class);
     	owners = typedQuery.getResultList();
 
 	    return owners;
