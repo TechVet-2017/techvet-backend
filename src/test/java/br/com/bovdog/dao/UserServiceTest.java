1730d26dd14eb9f0f85b87f62a0411fd1c7b2449
Flavio Costa <skingk.f@gmail.com>
2017-05-11 22:07:03 -0300

    Implement updateUserTest
    Related to issue #1
    Related to issue #2
    Related to issue #37
    
    Signed-off-by: Flavio Costa <skingk.f@gmail.com>
    Signed-off-by: leomeister <leomeister22@gmail.com>

--- src/test/java/br/com/bovdog/dao/UserServiceTest.java
+++ src/test/java/br/com/bovdog/dao/UserServiceTest.java
@@ -22,29 +22,29 @@ public class UserServiceTest {
 	private final String TEST_UNIT = "test-unit";
 	private UserService userService;
 	private DataAccessObject testDAO;
-	
+
 	@Before
 	public void setUp() {
 		user = setupUser();
 		testDAO = new DataAccessObject(TEST_UNIT);
 		userService = new UserService(testDAO);
 	}
-	
+
 	@After
 	public void clearDatabase() {
 		PersistenceHelper.clearDatabase();
 	}
-	
+
 	public User setupUser() {
 		User user = new User();
-		
+
 		user.setUserFullName("String");
 		user.setUserName("String");
 		user.setUserPassword("String");
-		
+
 		return user;
 	}
-	
+
 	@Test
 	public void createUserTest() {
 		int id = userService.createUser(user).getId();
@@ -56,17 +56,17 @@ public class UserServiceTest {
 		UriInfo ui = mock(UriInfo.class);
 		when(ui.getQueryParameters()).thenReturn(null);
 		List<User> users = new ArrayList<User>();
-		
+
 		for (int i = 0; i < 3; i++) {
 			User user = setupUser();
 			user.setUserName("User " + i);
 			user = testDAO.createObject(user);
 			users.add(user);
 		}
-		
+
 		assertEquals(users, userService.getAllUsers(ui));
 	}
-	
+
 	@Test
 	public void deleteUserTest() {
 		List<User> users = new ArrayList<User>();
@@ -78,8 +78,41 @@ public class UserServiceTest {
 		}
 
 		userService.deleteUser(users.get(0).getId());
-		
-		assertEquals(users.size() - 1, testDAO.getAllObjects(null, User.class).size());
+
+		assertEquals(users.size() - 1, testDAO.getAllObjects(null, User.class)
+				.size());
 	}
-	
+
+	@Test
+	public void updatePatientTest() {
+		User user = setupUser();
+		user = userService.createUser(user);
+
+		assertEquals(testDAO.getObjectById(user.getId(), User.class)
+				.getUserFullName(), user.getUserFullName());
+		
+		user.setUserFullName("FullNameUpdated");
+		user = userService.updateUser(user.getId(), user);
+		
+		assertEquals(testDAO.getObjectById(user.getId(), User.class)
+				.getUserFullName(), user.getUserFullName());
+
+		assertEquals(testDAO.getObjectById(user.getId(), User.class)
+				.getUserName(), user.getUserName());
+		
+		user.setUserFullName("NameUpdated");
+		user = userService.updateUser(user.getId(), user);
+		
+		assertEquals(testDAO.getObjectById(user.getId(), User.class)
+				.getUserName(), user.getUserName());
+
+		assertEquals(testDAO.getObjectById(user.getId(), User.class)
+				.getUserPassword(), user.getUserPassword());
+		
+		user.setUserFullName("PasswordUpdated");
+		user = userService.updateUser(user.getId(), user);
+		
+		assertEquals(testDAO.getObjectById(user.getId(), User.class)
+				.getUserPassword(), user.getUserPassword());
+	}
 }
