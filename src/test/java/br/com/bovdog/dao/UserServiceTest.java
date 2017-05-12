9eeef76db3beea47ef673d2f1d52a5b35874f8a7
leomeister <leomeister22@gmail.com>
2017-05-11 21:38:12 -0300

    Implement deleteUserTest
    Related-to-issue #1
    Related-to-issue #2
    Related-to-issue #37
    
    Signed-off-by: leomeister <leomeister22@gmail.com>
    Signed-off-by: Flávio Costa <skingk.f@gmail.com>
    Signed-off-by: Lucas Amoêdo <lucas_advc@outlook.com>

--- src/test/java/br/com/bovdog/dao/UserServiceTest.java
+++ src/test/java/br/com/bovdog/dao/UserServiceTest.java
@@ -67,4 +67,19 @@ public class UserServiceTest {
 		assertEquals(users, userService.getAllUsers(ui));
 	}
 	
+	@Test
+	public void deleteUserTest() {
+		List<User> users = new ArrayList<User>();
+		for (int i = 0; i < 3; i++) {
+			User user = setupUser();
+			user.setUserName("User " + i);
+			user = testDAO.createObject(user);
+			users.add(user);
+		}
+
+		userService.deleteUser(users.get(0).getId());
+		
+		assertEquals(users.size() - 1, testDAO.getAllObjects(null, User.class).size());
+	}
+	
 }
