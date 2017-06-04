0be6562b4de1899e58ad8dd595abdadce65d3b78
Luiz Guilherme Silva <do.guilherme@hotmail.com>
2017-06-04 10:19:46 -0300

    Create relationship between bathGrooming and patients
    Related to issue #21
    Related to issue #2
    
    Signed-off-by: Flavio Costa <skingk.f@gmail.com>
    Signed-off-by: Mateus Vieira <mateusvroriz6b@gmail.com>
    Signed-off-by: Luiz Guilherme Silva <do.guilherme@hotmail.com>

--- src/main/java/br/com/bovdog/bean/BathGrooming.java
+++ src/main/java/br/com/bovdog/bean/BathGrooming.java
@@ -1,8 +1,12 @@
 package br.com.bovdog.bean;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 
 @Entity
 public class BathGrooming {
@@ -10,6 +14,10 @@ public class BathGrooming {
 	@GeneratedValue
 	private int id;
 	private String serviceBathGrooming;
+	
+	// create relationship between bathAndGrooming and patient
+	@OneToMany(mappedBy="bathGrooming")
+	private List<Patient> patients;
 	
 	// getters and setters for BathGrooming attributes.
 	public int getId() {
