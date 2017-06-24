ed44b21522d73fae399b713cb6940400aebb2da3
Adailson Santos <adailson2@gmail.com>
2017-06-24 18:09:29 -0300

    Insert javadoc comments for ServiceInterface
    Signed-off-by: Adailson Santos <adailson2@gmail.com>
    Signed-off-by: Leonardo <leomeister22@gmail.com>

--- src/main/java/br/com/bovdog/service/ServiceInterface.java
+++ src/main/java/br/com/bovdog/service/ServiceInterface.java
@@ -1,5 +1,14 @@
 package br.com.bovdog.service;
-
+/**
+ * Interface that will be implemented by object services
+ * 
+ * @author adailson2, mateusvroriz, antoniocoj, SkiNgK, gustavo2795, iamferreirajp, leomeister, LucasAmoedo, luizguilherme5, 
+ * oliveiraMarcelo, varleysilva
+ * @version 1.0
+ *
+ */
 public interface ServiceInterface {
+	/** Constant that will be used for generic damage
+      */
 	public static final String TECHVET_UNIT = "techvet-unit";
 }
