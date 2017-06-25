8e892c5efbfbfef13ef191f43d55d8847f39c031
Flavio Costa <skingk.f@gmail.com>
2017-06-24 22:48:32 -0300

    Using the File Header
    Related to issue #2
    
    Signed-off-by: Flavio Costa <skingk.f@gmail.com>

--- src/main/java/br/com/bovdog/bean/Owner.java
+++ src/main/java/br/com/bovdog/bean/Owner.java
@@ -1,9 +1,19 @@
+/*
+ * File: Owner.java
+ * Description: Defines owner object and his attributes. 
+ */
 package br.com.bovdog.bean;
 
+// imports
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+/**
+ * Class that describes the owner, and contains the information and methods it requires.
+ * @author flavio
+ * @version 1.0
+ */
 @Entity
 public class Owner {
 
@@ -22,78 +32,194 @@ public class Owner {
 	private String city;
 	private String district;
 	
-
-	//Create relationship between entities
-	
+	/**
+	 * Identifier owner get method.
+	 * @return id
+	 */
 	public int getId() {
 		return this.id;
 	}
-	public String getNeighborhood() {
-		return this.neighborhood;
-	}
-	public void setNeighborhood(String neighborhood) {
-		this.neighborhood = neighborhood;
-	}
-	public String getCity() {
-		return this.city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
+	
+	/**
+	 * Identifier owner set method. 
+	 * @param ownerId
+	 */
 	public void setId(int ownerId) {
 		this.id = ownerId;
 	}
+	
+	/**
+	 * Owner's neighborhood get method.
+	 * @return neighborhood
+	 */
+	public String getNeighborhood() {
+		return this.neighborhood;
+	}
+	
+	/**
+	 * Owner's neighborhood set method.
+	 * @param neighborhood
+	 */
+	public void setNeighborhood(String neighborhood) {
+		this.neighborhood = neighborhood;
+	}
+	
+	/**
+	 * Owner's city get method.
+	 * @return city
+	 */
+	public String getCity() {
+		return this.city;
+	}
+	
+	/**
+	 * Owner's city set method.
+	 * @param city
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	/**
+	 * Owner's cpf get method.
+	 * @return cpf
+	 */
 	public String getCpf() {
 		return this.cpf;
 	}
+	
+	/**
+	 * Owner's cpf set method.
+	 * @param cpf
+	 */
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+	
+	/**
+	 * Owner name get method.
+	 * @return ownerName
+	 */
 	public String getOwnerName() {
 		return this.ownerName;
 	}
+	
+	/**
+	 * Owner name set method.
+	 * @param ownerName
+	 */
 	public void setOwnerName(String ownerName) {
 		this.ownerName = ownerName;
 	}
+	
+	/**
+	 * Owner last name get method.
+	 * @return ownerLastName
+	 */
 	public String getOwnerLastName() {
 		return this.ownerLastName;
 	}
+	
+	/**
+	 * Owner last name set method.
+	 * @param ownerLastName
+	 */
 	public void setOwnerLastName(String ownerLastName) {
 		this.ownerLastName = ownerLastName;
 	}
+	
+	/**
+	 * Owner's phone number get method.
+	 * @return phoneNumber
+	 */
 	public String getPhoneNumber() {
 		return this.phoneNumber;
 	}
+	
+	/**
+	 * Owner's phone number set method.
+	 * @param phoneNumber
+	 */
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
+	
+	/**
+	 * Owner's zip code get method.
+	 * @return zipCode
+	 */
 	public String getZipCode() {
 		return this.zipCode;
 	}
+	
+	/**
+	 * Owner's zip code set method.
+	 * @param zipCode
+	 */
 	public void setZipCode(String zipCode) {
 		this.zipCode = zipCode;
 	}
+	
+	/**
+	 * Owner's district get method.
+	 * @return district
+	 */
 	public String getDistrict() {
 		return this.district;
 	}
+	
+	/**
+	 * Owner's district set method.
+	 * @param district
+	 */
 	public void setDistrict(String district) {
 		this.district = district;
 	}
+	
+	/**
+	 * Owner's public place get method.
+	 * @return publicPlace
+	 */
 	public String getPublicPlace() {
 		return this.publicPlace;
 	}
+	
+	/**
+	 * Owner's public place set method.
+	 * @param publicPlace
+	 */
 	public void setPublicPlace(String publicPlace) {
 		this.publicPlace = publicPlace;
 	}
+	
+	/**
+	 * Owner's address number get method.
+	 * @return addressNumber
+	 */
 	public Long getAddressNumber() {
 		return this.addressNumber;
 	}
+	
+	/**
+	 * Owner's address number set method.
+	 * @param addressNumber
+	 */
 	public void setAddressNumber(Long addressNumber) {
 		this.addressNumber = addressNumber;
 	}
+	
+	/**
+	 * Owner's complement get method.
+	 * @return complement
+	 */
 	public String getComplement() {
 		return this.complement;
 	}
+	
+	/**
+	 * Owner's complement set method.
+	 * @param complement
+	 */
 	public void setComplement(String complement) {
 		this.complement = complement;
 	}
