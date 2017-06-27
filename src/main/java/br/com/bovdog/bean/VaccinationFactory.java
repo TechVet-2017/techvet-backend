f821699eb300985881b3e78be946be816e5332d7
João Paulo Alves Ferreira <iamferreirajp@gmail.com>
2017-06-26 23:15:31 -0300

    Commenting using Javadoc

--- src/main/java/br/com/bovdog/bean/VaccinationFactory.java
+++ src/main/java/br/com/bovdog/bean/VaccinationFactory.java
@@ -1,7 +1,16 @@
 package br.com.bovdog.bean;
 
+/**
+ * Class that implements the clinicalrecord and return a instance of clinical record.
+ * @author João Paulo
+ * @version 1.0
+ */
 public class VaccinationFactory implements ClinicalRecordFactory {
 
+	/**
+	*Return the instance
+	*@return ClinicalRecordVaccination clinicalRecordVaccination
+	*/
 	@Override
 	public ClinicalRecord createClinicalRecord() {
 		ClinicalRecordVaccination clinicalRecordVaccination = new ClinicalRecordVaccination();
